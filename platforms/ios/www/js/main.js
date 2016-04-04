278829025d6c672328d89cf85978c73c73401100
José Becerra <antonio.becerra@grupomedios.com>
2016-04-04 18:42:22 -0500

    asdf

--- platforms/ios/www/js/main.js
+++ platforms/ios/www/js/main.js
@@ -10,4 +10,21 @@ $(document).ready(function(){
 		localStorage.setItem('session_id', '');
 		window.location = 'index.html';
 	});
+	$('#busqueda input').keyup(function() { 
+		url = "http://ygiis.mx/api?m=busqueda&u="+$(this).val();
+		$.ajax({
+			url : url,
+			success : function(data){
+					$('#resultadosbusqueda').html('');
+				$.each(data, function( index, value ) {
+					output = '<div class="bus"><a href="#" class="botonbusqueda" rel="'+value.id+'">'+value.name+'</a></div>';
+					$('#resultadosbusqueda').append(output);
+				})
+			}
+		})
+	});
+	$(document).on("click",".botonbusqueda",function(event){
+		localStorage.setItem('busqueda_usuario', $(this).attr('rel'));
+		window.location = "perfil-otro.html";
+	});
 })
